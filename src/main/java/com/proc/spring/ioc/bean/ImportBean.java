2401871c4fc634901594a73545c2b585c13b618a
fjh <635286502@qq.com>
2020-05-12 01:15:57 +0800

    add import label

---
+++ src/main/java/com/proc/spring/ioc/bean/ImportBean.java
@@ -0,0 +1,11 @@
+package com.proc.spring.ioc.bean;
+
+/**
+ * Created by fangjianhui on 2020/5/12.
+ */
+public class ImportBean {
+
+    public void call(){
+        System.out.println("i am import bean!");
+    }
+}
