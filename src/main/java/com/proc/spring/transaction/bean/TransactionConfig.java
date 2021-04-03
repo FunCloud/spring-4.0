2ac148738cc63917ef8f9a040ae85a19ec0b84fb
fjh <635286502@qq.com>
2021-04-04 01:15:14 +0800

    spring源码之Aop 优化aop事务代码

--- src/main/java/com/proc/spring/transaction/bean/TransactionConfig.java
+++ src/main/java/com/proc/spring/transaction/bean/TransactionConfig.java
@@ -1,10 +1,13 @@
 package com.proc.spring.transaction.bean;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
@@ -14,15 +17,36 @@ import javax.sql.DataSource;
  * @Description:
  */
 @Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:transaction/druid.properties")
 public class TransactionConfig {
+
+    @Value("${druid.driverClassName}")
+    private String driverClassName;
+
+    @Value("${druid.url}")
+    private String url;
+
+    @Value("${druid.username}")
+    private String username;
+
+    @Value("${druid.password}")
+    private String password;
 
     @Bean
     public DataSource dataSource() {
-        // configure and return the necessary JDBC DataSource
-        return new DruidDataSource();
+        System.out.println("driverClassName====> " + driverClassName);
+        System.out.println("url====> " + url);
+        System.out.println("username====> " + username);
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setDriverClassName(driverClassName);
+        druidDataSource.setUrl(url);
+        druidDataSource.setUsername(username);
+        druidDataSource.setPassword(password);
+        return druidDataSource;
     }
 
-    @Bean("transactionManager")
+    @Bean
     public PlatformTransactionManager txManager() {
         return new DataSourceTransactionManager(dataSource());
     }
