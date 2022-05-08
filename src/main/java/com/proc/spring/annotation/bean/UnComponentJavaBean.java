de81f2e6398c02dcc56667a4e142aae75677d95d
fjh <635286502@qq.com>
2022-05-08 22:51:38 +0800

    学习findCandidateComponents(basePackage)方法

---
+++ src/main/java/com/proc/spring/annotation/bean/UnComponentJavaBean.java
@@ -0,0 +1,10 @@
+package com.proc.spring.annotation.bean;
+
+/**
+ * @Auther: 方建辉
+ * @Date: 2022/5/8 22:17
+ * @Description:
+ */
+// 非spring 容器管理bean，测试 Set<BeanDefinition> candidates = findCandidateComponents(basePackage)方法，过滤扫描@Component注解类
+public class UnComponentJavaBean {
+}
